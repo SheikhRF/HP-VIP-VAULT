d76edfbf4de88343996771952f7cec673ce68b89
SheikhRF <sheikh.rf@oundleschool.org.uk>
2026-01-07 00:23:37 +0000

    building new car page for api use

--- hp-vip-vault/proxy.ts
+++ hp-vip-vault/proxy.ts
@@ -1,5 +1,6 @@
 // proxy.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
 
 /**
   *public routes (that don't require authentication)
@@ -14,6 +15,24 @@ const isPublicRoute = createRouteMatcher([
   *Create the middleware to protect the routes
  */
 export default clerkMiddleware(async(auth, req) => {
+  const { pathname } = req.nextUrl;
+  const isApiRoute = pathname.startsWith("/api") || pathname.startsWith("/trpc");
+
+  // redirect apis diferently to normal pages
+  if (isApiRoute) {
+    const { isAuthenticated } = await auth();
+
+    if (!isAuthenticated) {
+      return NextResponse.json(
+        { ok: false, error: "Not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    // authenticated -> allow request through
+    return NextResponse.next();
+  }
+
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
